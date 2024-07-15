0bc51db7773e2e95a9fb44392d355d611fe8ae24
yizhong187 <yizhong187@gmail.com>
2024-07-15 13:24:14 +0800

    added chat screen

--- navigation/types.ts
+++ navigation/types.ts
@@ -3,7 +3,7 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 // Defines the parameters that are passed into the screens
 export type RootStackParamList = {
   Home: undefined;
-  Chat: undefined;
+  Chat: { chatId: number };
   Start: undefined;
   StudentLogin: undefined;
   StudentRegister: undefined;
